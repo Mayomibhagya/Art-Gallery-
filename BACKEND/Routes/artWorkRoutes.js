25fdd9d02562b0bde0fda6a3a1e83b91d01a85b6
Mayomibhagya <mayomibhagya.mb@gmail.com>
2025-10-01 10:35:29 +0530

    Add files via upload

---
+++ BACKEND/Routes/artWorkRoutes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const artWork = require('../Models/artWorkModel'); //Insert Model
+const artWorkController = require('../Controllers/artWorkController'); //Insert Controller
+
+router.get("/",artWorkController.getAllArtWorks); //Get All ArtWorks
+router.post("/",artWorkController.addArtWorks); //Add ArtWorks
+router.get("/:id",artWorkController.getById); //Get ArtWorks by ID
+router.put("/:id",artWorkController.updateArtWork); //Update ArtWorks
+router.patch("/updateart/:id",artWorkController.updateArtWork);
+router.delete("/:id",artWorkController.deleteArtWork); //Delete ArtWorks
+
+//Export 
+module.exports = router;
---
+++ BACKEND/Routes/inquiryRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+//model inseting is here 
+const inquiryM = require('../Models/InquiryModel');
+//controller inserting is here
+const inquiryC = require('../Controllers/InquiryController');
+
+
+//model inserting is here 
+//controller inserting is here.
+
+router.get("/", inquiryC.getAllInquiries); //Get All Inquiries
+router.post("/", inquiryC.addInquiry); //Insert new Inquiries
+router.post("/:id",  inquiryC.getInquiryById); //Get Inquiry by ID
+router.post("/i/:email",  inquiryC.getInquiryByEmail); //Get Inquiry by ID
+router.put("/:id", inquiryC.updateInquiry); //Update Inquiry
+router.delete("/:id", inquiryC.deleteInquiry); //Delete Inquiry
+
+
+module.exports = router;
